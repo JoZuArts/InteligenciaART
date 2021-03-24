74758327f160d7780c37fde9a133b8a0bd92d8dc
JoZuArts <Nerfdac22@gmail.com>
2021-03-24 11:13:31 -0600

    cambios de codigos
    acabo de poner los comentarios a cada carpeta

--- AGENTE_TORRETA/Assets/Scripts/EstadoAtacar.cs
+++ AGENTE_TORRETA/Assets/Scripts/EstadoAtacar.cs
@@ -1,31 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+/* este codigo es importante recordar ya que es 
+  el estado de atacar y no se puede olvidar
+ */
 
-public class EstadoAtacar : InstruccionesFSM
+public class EstadoAtacar : InstruccionesFSM    //codigo estado atacar instrucciones 
 {
-    float nextFire;
-    float rateFire = 1f;
+    float nextFire;                          // metodo para saber el siguiente disparo
+    float rateFire = 1f;                         // dejar de disparar
 
-    public override void EnterState(TorretaEstatica torreta)
+    public override void EnterState(TorretaEstatica torreta)               
     {
-        Debug.Log("Entro estado atacar!");
-        torreta.currentState = AgentState.Attack;
+        Debug.Log("Entro estado atacar!");                               //metodo para entrar en estado de ataque 
+        torreta.currentState = AgentState.Attack;                    // ataque hacia agente
 
     }
 
     public override void UpdateState(TorretaEstatica torreta)
     {
-        if(torreta.playerDetected == true)
+        if(torreta.playerDetected == true)                                       // cuando detecta el jugador
         {
             var lookRotation = Quaternion.LookRotation(torreta.playerTarget.transform.position - torreta.transform.position);
             torreta.transform.rotation = Quaternion.Slerp(torreta.transform.rotation, lookRotation, torreta.speedRotation * Time.deltaTime);
 
             //Disparar
         }
-        else
+        else     // si no
         {
-            torreta.TransitionToState(torreta.idleState);
+            torreta.TransitionToState(torreta.idleState);          //movimiento de torreta 
         }
     }
 }
--- AGENTE_TORRETA/Assets/Scripts/EstadoRotacion.cs
+++ AGENTE_TORRETA/Assets/Scripts/EstadoRotacion.cs
@@ -2,24 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EstadoRotacion : InstruccionesFSM
+public class EstadoRotacion : InstruccionesFSM           //rotar la torreta chevere 
 {
-    public override void EnterState(TorretaEstatica torreta)
+    public override void EnterState(TorretaEstatica torreta)         // es el iniciador de la torreta
     {
-        Debug.Log("Entro a estado de rotación");
-        torreta.currentState = AgentState.Rotation;
+        Debug.Log("Entro a estado de rotación");                            // codigo de inicio 
+        torreta.currentState = AgentState.Rotation;                      // un codigo de current 
     }
 
-    public override void UpdateState(TorretaEstatica torreta)
+    public override void UpdateState(TorretaEstatica torreta)                  //nombre del codigo
     {
-        torreta.transform.rotation = Quaternion.Slerp(torreta.transform.rotation,
-        Quaternion.Euler(torreta.angles[torreta.angleIndex]), Time.deltaTime * torreta.speedRotation);
+        torreta.transform.rotation = Quaternion.Slerp(torreta.transform.rotation,              
+        Quaternion.Euler(torreta.angles[torreta.angleIndex]), Time.deltaTime * torreta.speedRotation);           //un codigo de rotacion 
 
-        if(torreta.transform.eulerAngles.y >= (torreta.angles[torreta.angleIndex].y - 1))
+        if(torreta.transform.eulerAngles.y >= (torreta.angles[torreta.angleIndex].y - 1))                         //transformacion de torreta 
         {
-            Debug.Log("Llegue al angulo destino");
-            torreta.angleIndex = (torreta.angleIndex + 1) % torreta.angles.Length;
-            torreta.TransitionToState(torreta.idleState);
+            Debug.Log("Llegue al angulo destino");                                 //codigo de angulo destino 
+            torreta.angleIndex = (torreta.angleIndex + 1) % torreta.angles.Length;                                    //importante recordar esto
         }
     }
 }
