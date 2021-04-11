55f0a1fc64fc653601a40b931b7456ae2bb38e43
JoZuArts <Nerfdac22@gmail.com>
2021-04-11 00:32:14 -0500

    guardar archivos
    cositas descargdaas

--- AGENTE_TORRETA/Assets/Scripts/agente dinamico/agentedinamico.cs
+++ AGENTE_TORRETA/Assets/Scripts/agente dinamico/agentedinamico.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using 
+
 
 public class agentedinamico : MonoBehaviour
 {
- navmeshAgent agent:
+
     // Start is called before the first frame update
     void Start()
     {
