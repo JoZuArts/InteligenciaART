d335ff6139640b4e5679bcc09157f751b274b892
JoZuArts <Nerfdac22@gmail.com>
2021-04-11 23:12:17 -0500

    codigos3ervideo
    poner mas atencion a los codigos y fallas

--- AGENTE_TORRETA/Assets/Scripts/agente dinamico/agentedinamico.cs
+++ AGENTE_TORRETA/Assets/Scripts/agente dinamico/agentedinamico.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 public class agentedinamico : MonoBehaviour
 {
+    NavMeshAgent agent;
+
+    public Transform waypoint;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.SetDestination(waypoint.position);
     }
+
 
     // Update is called once per frame
     void Update()
